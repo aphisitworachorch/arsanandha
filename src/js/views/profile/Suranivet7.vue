49bc61409b49e0c694587f835e9d1928f6334460
Asanan Aphisitworachorch <thaixperiasp@gmail.com>
2020-05-02 12:17:12 +0700

    add s7 image
    Signed-off-by: Asanan Aphisitworachorch <thaixperiasp@gmail.com>

--- src/js/views/profile/Suranivet7.vue
+++ src/js/views/profile/Suranivet7.vue
@@ -1,10 +1,60 @@
 <template>
-
+    <div>
+        <div class="uk-container">
+            <img width="65%" style="text-align: center" :src="s7Image"/>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "Suranivet7"
+        name: "Suranivet7",
+        methods: {
+            listFriends: () => {
+
+                console.table(data.friendList)
+            }
+        },
+        data: () => {
+            return{
+                s7Image : require('../../../../img/s7/suranivet7sharp8.jpg'),
+                friendList: {
+                    "nameOfFriends":[
+                        "Por Chainarin" ,
+                        "Kanomjeen Sawapa" ,
+                        "Tom Wuttichai" ,
+                        "Bori Boripat" ,
+                        "Off Kachanon" ,
+                        "C-Tose Narongsak" ,
+                        "Group Jirattiti" ,
+                        "Ta Ronnarit" ,
+                        "Som Methathorn" ,
+                        "Champ Worawich" ,
+                        "Tin Tin" ,
+                        "Kao Nidhisin" ,
+                        "Ping Aekkasit" ,
+                        "Pu Natthasit" ,
+                        "Boy Apichet" ,
+                        "Pleum Nawapat" ,
+                        "P' Supphakorn" ,
+                        "Frame Thanapat" ,
+                        "Jab Namchai" ,
+                        "Men Nakkarin" ,
+                        "Nutcha Surasak" ,
+                        "Benz Thammanun" ,
+                        "Bomb Natthawut" ,
+                        "Brand Paramet" ,
+                        "Tan Rames" ,
+                        "Ton Jirawat" ,
+                        "Oat Chatuphon" ,
+                        "Art Pratchaya"
+                    ]
+                }
+            }
+        },
+        mounted() {
+            this.listFriends()
+        }
     }
 </script>
 
