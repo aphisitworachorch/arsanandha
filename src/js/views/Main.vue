3654e9f59832d41011b80a65ff9079fa4ec1b716
Asanan Aphisitworachorch <thaixperiasp@gmail.com>
2020-05-02 09:34:53 +0700

    default font
    Signed-off-by: Asanan Aphisitworachorch <thaixperiasp@gmail.com>

--- src/js/views/Main.vue
+++ src/js/views/Main.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <vk-navbar>
-            <vk-navbar-logo slot="center"><iframe :src="imgX"/></vk-navbar-logo>
+            <vk-navbar-logo slot="center">
+                <iframe :src="imgX"/>
+            </vk-navbar-logo>
         </vk-navbar>
         <router-view></router-view>
         <footer style="text-align: center">(C) Asanan Aphisitworachorch</footer>
@@ -11,14 +13,20 @@
 <script>
     export default {
         name: "Main",
-        data: function(){
-            return{
+        data: function () {
+            return {
                 imgX: require('../../../img/logo-01.svg')
             }
         }
     }
 </script>
 
-<style scoped>
+<style lang="sass" scoped>
+    @import url('https://fonts.googleapis.com/css?family=K2D&display=swap&subset=thai')
 
+    body
+        font-family: 'K2D', sans-serif
+
+    h1, h2, h3, h4, h5, p
+        font-family: 'K2D', sans-serif
 </style>
