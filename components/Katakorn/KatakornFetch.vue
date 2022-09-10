f20d5d27c24fd6fe21a09a208324c5363ef33d54
Arsanandha Abhisidvorachodi <arsanandha.ap@gmail.com>
2022-09-10 12:50:57 +0700

    Add Data Exporter

--- components/Katakorn/KatakornFetch.vue
+++ components/Katakorn/KatakornFetch.vue
@@ -34,6 +34,20 @@
               <button class="btn btn-info" @click="viewData(fD.data)">ดูข้อมูล</button>
             </td>
           </tr>
+          <tr>
+            <td></td>
+            <td>
+              <download-excel
+                class="btn btn-info"
+                :data="formatData(katakornRegisterData)"
+                :fields="json_fields"
+              >
+                ดาวน์โหลดรายชื่อทั้งหมด
+              </download-excel>
+            </td>
+            <td></td>
+            <td></td>
+          </tr>
           </tbody>
         </table>
       </div>
@@ -44,6 +58,10 @@
 
 <script>
 import Swal from "sweetalert2";
+import {DateTime} from "luxon";
+import * as PhoneFormat from 'phone-number-formats';
+import * as _ from 'lodash';
+
 export default {
   name: "KatakornFetch",
   async fetch(){
@@ -101,7 +119,27 @@ export default {
           name: "ปีอื่นๆ",
           id: "year_other"
         }
-      ]
+      ],
+      json_fields:{
+        'ชื่อจริง':'name',
+        'นามสกุล':'lastname',
+        'เลขประจำตัวนักศึกษา':'studentid',
+        'ชื่อเล่น':'nickname',
+        'อายุ':'age',
+        'วันเกิด': 'birthdayformat',
+        'ส่วนสูง':'height',
+        'น้ำหนัก':'weight',
+        'เบอร์โทรศัพท์':'telephoneFormat',
+        'ชื่อเฟส':'social_link',
+        'ชื่อไอจี':'instagram_link',
+        'ปีที่ศึกษา':'yearstudy',
+        'สำนักวิชา':'facultyname',
+        'เคยเป็นคทากรมาก่อนหน้านี้หรือไม่':'everkatakorn',
+        'เหตุผลที่อยากเป็นคทากร':'reason_to_be_katakorn',
+        'ทำไมถึงอยากเป็นคทากร':'why_you_want_to_be_katakorn',
+        'ความสามารถพิเศษ':'abilities',
+        'รูปภาพ':'photo.url'
+      }
     }
   },
   methods:{
@@ -146,6 +184,31 @@ export default {
         icon: 'info',
         html: formattedData
       });
+    },
+    formatData(data) {
+      return _.uniqBy(data.map(finalData => {
+        const facData = this.yearData.filter(dp => dp.id === finalData.data.faculty)
+        const yearDataM = this.yearInfo.filter(yI => yI.id === finalData.data.year_study)
+        return {
+          name: finalData.first_name,
+          lastname: finalData.last_name,
+          photo_link: finalData?.photo?.url,
+          studentid: finalData.data.student_id,
+          everkatakorn: finalData.ever_katakorn ? "ใช่" : "ไม่",
+          yearstudy: yearDataM.shift().name,
+          facultyname: facData.shift().name,
+          birthdayformat: DateTime.fromISO(finalData.data.birthday).toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY,{
+            locale:'th-TH'
+          }),
+          telephoneFormat: new PhoneFormat(finalData.data.telephone).format({
+            type:'domestic'
+          }),
+          ...finalData.data,
+        }
+      }),'studentid')
+    },
+    getFileName() {
+      return `KATAKORN-LIST-${DateTime.local().toString()}`
     }
   }
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,8 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '~/plugins/vue-kinesis.js',
-    '~/plugins/vue-typed.js'
+    '~/plugins/vue-typed.js',
+    '~/plugins/vue-excel.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
